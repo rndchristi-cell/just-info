88fd9b87de9867838b71322ead3b2bdeb6fc570a
rndchristi-cell <rendychristi@gmail.com>
2026-09-06 21:26:09 +0700

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,12 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
+
+    // Default membuat artikel lama tetap valid
+    // meskipun belum memiliki field category atau tags.
+    category: z.string().optional().default('Umum'),
+    tags: z.array(z.string()).optional().default([]),
+
     bannerImage: z.string().optional(),
     bannerUrl: z.string().optional(),
     bannerPosisi: z
