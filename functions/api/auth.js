72e3fdd3518d816b0e8ecac7a07851aa38330ea3
rndchristi-cell <rendychristi@gmail.com>
2026-09-06 11:40:32 +0700

    Create functions/api/auth.js

---
+++ functions/api/auth.js
@@ -0,0 +1,27 @@
+export async function onRequest(context) {
+    const {
+        request,
+        env,
+    } = context;
+
+    const client_id = env.GITHUB_CLIENT_ID;
+
+    try {
+        const url = new URL(request.url);
+        const redirectUrl = new URL('https://github.com/login/oauth/authorize');
+        redirectUrl.searchParams.set('client_id', client_id);
+        redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/callback');
+        redirectUrl.searchParams.set('scope', 'repo user');
+        redirectUrl.searchParams.set(
+            'state',
+            crypto.getRandomValues(new Uint8Array(12)).join(''),
+        );
+        return Response.redirect(redirectUrl.href, 301);
+
+    } catch (error) {
+        console.error(error);
+        return new Response(error.message, {
+            status: 500,
+        });
+    }
+}
