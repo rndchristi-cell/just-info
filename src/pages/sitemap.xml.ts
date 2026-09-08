940bde81c5f31797d3e9f00ee19f4c62a50528c0
rndchristi-cell <rendychristi@gmail.com>
2026-09-08 16:46:59 +0700

    Add dynamic XML sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,24 @@
+import { getCollection } from 'astro:content';
+
+const escapeXml = (value: string) =>
+  value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&apos;');
+
+export async function GET({ site }: { site: URL }) {
+  const posts = await getCollection('blog');
+  const urls = [
+    site.href,
+    ...posts.map((post) => new URL(`${post.id.replace(/\.md$/, '')}/`, site).href),
+  ];
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((url) => `  <url><loc>${escapeXml(url)}</loc></url>`).join('\n')}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600',
+    },
+  });
+}
