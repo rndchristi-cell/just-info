720e913ab75970658ea532965d37df35af9d3bcf
rndchristi-cell <rendychristi@gmail.com>
2026-09-13 09:28:10 +0700

    Include Produk Rekomendasi in sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -8,6 +8,7 @@ export async function GET({ site }: { site: URL }) {
   const staticPaths = [
     '',
     'artikel/',
+    'produk-rekomendasi/',
     'tentang/',
     'kontak/',
     'kebijakan-privasi/',
