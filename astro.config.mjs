cf37f0019b06143eafc8822be0cd7458ff7f68a7
rndchristi-cell <rendychristi@gmail.com>
2026-09-18 18:50:24 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap'; // 1. Add this import
 
 export default defineConfig({
   site: 'https://yourinfo.my.id',
+  integrations: [sitemap()],            // 2. Add this array
 });
